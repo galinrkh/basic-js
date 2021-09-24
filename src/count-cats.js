cd462b5c7c80270d7fb30ba77fc3fbb0225b3520
Rustam Galin <galinrkh@gmail.com>
2021-09-25 03:19:21 +0500

    fix: solved count cats problem

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+export default function countCats(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let amount = 0;
+  // let i, j;
+  let row = matrix.length;
+  // let col = matrix[i].length;
+
+  for (let i = 0; i < row; i++) {                   //перебор по-рядно
+    for (let j = 0; j < matrix[i].length; j++) {    //перебор по-колонно
+      if (matrix[i][j] === '^^') {                  //проверка на совпадение, если совпало, то
+        amount += 1;                                //увеличение суммы на 1
+      }
+    }
+  }
+  return amount;                                    //возврат полученной суммы
+
 }
--- src/st-get-email-domain.js
+++ src/st-get-email-domain.js
@@ -10,18 +10,16 @@ import { NotImplementedError } from '../extensions/index.js';
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-export default function getEmailDomain(/* email */) {
+export default function getEmailDomain(email) {
 
   let result = '';
-  let i;
-  for (i = str.length; i > 0; i--) {
+  let str = email
+  for (let i = str.length; i > 0; i--) {
     if (str[i] === '@') {
       result = str.slice(i + 1);
       break;
     }
   }
-  return `'${result}'`;
-  //  console.log(`'${result}'`);
-
+  // return `'${result}'`;
+  console.log(`'${result}'`);
 }
-
