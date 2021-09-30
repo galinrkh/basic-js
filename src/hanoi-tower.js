6d3c8f0b968aaeba1a8809c0b46cdbf9a395fe94
Rustam Galin <galinrkh@gmail.com>
2021-10-01 00:59:50 +0500

    feat: solved hanoi-tower and fixed get_email_domain

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,7 +14,17 @@ import { NotImplementedError } from '../extensions/index.js';
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-export default function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function calculateHanoi(disksNumber, turnsSpeed) {
+
+  // объявление переменных
+  let turns;
+  let seconds;
+
+  // решение головоломки
+  turns = 2 ** disksNumber - 1;  // вычисление кол-ва туров по алгоритму turns=2^n-1
+  seconds = Math.floor(3600 / turnsSpeed * turns);  // нахождение времени для решения в секундах, т.к. входные данные в часах
+  return { turns, seconds };  // вернуть полученный результат вычислений
+
+};
+
+
--- src/st-get-email-domain.js
+++ src/st-get-email-domain.js
@@ -12,14 +12,17 @@ import { NotImplementedError } from '../extensions/index.js';
  */
 export default function getEmailDomain(email) {
 
+  //объявление переменной
   let result = '';
-  let str = email
-  for (let i = str.length; i > 0; i--) {
-    if (str[i] === '@') {
-      result = str.slice(i + 1);
-      break;
+  // let str = email;
+  // for (let i = 0; email.length > 0; i++) { //поиск с головы не верен, т.к. оказывается может много @
+
+  for (let i = email.length; i > 0; i--) {  // прохождение циклом с конца емейла до выполнения условия в {}
+    if (email[i] === '@') {                 // проверка условия (поиск @), чтобы зайти в {}
+      result = email.slice(i + 1);          // извлечение элементов из емейла от i+1 до конца. i-тый = @, а он нам не нужен, поэтому i+1
+      break;                                // прерывание и выход из цикла
     }
   }
-  // return `'${result}'`;
-  console.log(`'${result}'`);
+  return result;                            // вернуть полученный результат
+  // console.log(`'${result}`);
 }
