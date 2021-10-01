3a40d5ba5097a2142b7fa410e571f164e85be910
Rustam Galin <galinrkh@gmail.com>
2021-10-01 16:07:50 +0500

    feat: solved st-delete-digit

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,25 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function deleteDigit(n) {
+
+  // объявление переменных
+  let str = n.toString();  // перевод n в строку
+  let maxNumber = 0;
+  let interim = 0;
+
+  for (let i = 0; i < str.length; i++) {      // перебор цифр циклом
+
+    interim = str.slice(0, i) + str.slice(i + 1); // получение промежуточного числа из 2 цифр
+
+    // console.log(typeof (interim));
+    interim = Number(interim);        //перевод строчного числа в числовой тип
+    // console.log(typeof (interim));
+    if (maxNumber < interim) {        //сравнение полученного числа с максимальным, если это число больше максимального, то входим в {}
+      maxNumber = interim;            //присваиваем полученное число максимальному
+    }
+  }
+  return maxNumber;                   //вернуть максимальное число
+
+
 }
