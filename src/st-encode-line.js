498c5bb53f4c1548822938535ee4bf823cf2b9eb
Rustam Galin <galinrkh@gmail.com>
2021-10-03 01:09:13 +0500

    feat: solved st-encode-line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,34 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine( str ) {
+
+
+  if (str.length === 0) {   // проверяем строка пустая или нет
+    return '';              // если пустая, то возвращаем ''
+  }                         // если нет, то идем дальше
+
+  let sum = 1;              // т.к. строка не пустая, отсчет количества элементов начинаем с 1
+  let encodeStr = '';       // начальная наша закодированная строка
+
+  for (let i = 0; i < str.length; i++) {              // проходим циклом по элементам строки
+    if (str[i] === str[i + 1]) {                      // проверяем, если элемент строки совпадает с последующим элементом,
+      sum += 1;                                       // то сумму увеличиваем на 1
+    } else {                                          // иначе
+      if (str[i] !== str[i + 1]) {                    // проверяем, если элемент строки не совпадает с последующим элементом,
+        if ((encodeStr = sum + encodeStr) !== 1) {    // и проверяем закодированная строка не равна 1
+          encodeStr = encodeStr + `${sum}${str[i]}`;  // прибавляем к закодированной строке количество и букву
+          // console.log(encodeStr);                     // проверка что получилось
+          sum = 1;                                    // обновляем количество до 1
+        } else {                                      // иначе
+          encodeStr = encodeStr + `{str[i]}`;         // прибавляем к закодированной строке только букву
+          // console.log(encodeStr);                     // проверка что получилось
+        }
+      }    
+    }                                    
+  }
+  // console.log(encodeStr);                             // проверка что получилось
+  return encodeStr;                                   // вернуть полученный результат
+  
+
 }
